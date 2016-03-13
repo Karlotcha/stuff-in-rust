3da11bcbe8b0f65bbd4523d847c7bb36b3dff766
Karlo <karlo.hoa@gmail.com>
2016-03-13 15:54:23 +0000

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod my_functions;
+pub mod sorts;
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,6 @@
 extern crate stuff;
 use stuff::my_functions::*;
+use stuff::sorts::*;
 
 #[test]
 fn test_increment() {
@@ -26,4 +27,41 @@ fn test_power() {
   assert_eq!(32, power(2,5));
   assert_eq!(1, power(2,0));
   assert_eq!(2, power(2,1));
+}
+
+// TODO - DRY these tests
+#[test]
+fn test_selection_sort() {
+  let v = vec![13, 25, 3, 4543, 5, 0, 5, 1, 999];
+  let ordered_v = vec![0, 1 ,3 ,5 , 5, 13, 25, 999, 4543];
+
+  assert_eq!(ordered_v, selection_sort(&v));
+  // assert_eq!([], selection_sort(&[]));
+}
+
+#[test]
+fn test_insertion_sort() {
+  let v = vec![13, 25, 3, 4543, 5, 0, 5, 1, 999];
+  let ordered_v = vec![0, 1 ,3 ,5 , 5, 13, 25, 999, 4543];
+
+  assert_eq!(ordered_v, insertion_sort(&v));
+  // assert_eq!([], selection_sort(&[]));
+}
+
+#[test]
+fn test_merge_sort() {
+  let v = vec![13, 25, 3, 4543, 5, 0, 5, 1, 999];
+  let ordered_v = vec![0, 1 ,3 ,5 , 5, 13, 25, 999, 4543];
+
+  assert_eq!(ordered_v, merge_sort(&v));
+  // assert_eq!([], selection_sort(&[]));
+}
+
+#[test]
+fn test_quick_sort() {
+  let v = vec![13, 25, 3, 4543, 5, 0, 5, 1, 999];
+  let ordered_v = vec![0, 1 ,3 ,5 , 5, 13, 25, 999, 4543];
+
+  assert_eq!(ordered_v, merge_sort(&v));
+  // assert_eq!([], selection_sort(&[]));
 }
