70e4252ae00dc58c66d9d8ac913d72df11241e53
Karlo <karlo.hoa@gmail.com>
2016-03-13 14:46:19 +0000

    adding semicolons

--- src/sorts/mod.rs
+++ src/sorts/mod.rs
@@ -10,7 +10,7 @@ pub fn selection_sort(v: &Vec<i32>) -> Vec<i32> {
     let mut index_min = i;
 
     for j in i..n {
-      if new_v[j] < new_v[index_min] { index_min = j }
+      if new_v[j] < new_v[index_min] { index_min = j; }
     }
 
     new_v = swap(new_v, i, index_min);
@@ -23,7 +23,7 @@ pub fn insertion_sort(v: &Vec<i32>) -> Vec<i32> {
   let mut new_v = vec!();
   let n = new_v.len();
 
-  for e in v { new_v = insert(new_v, *e) }
+  for e in v { new_v = insert(new_v, *e); }
 
   new_v
 }
@@ -70,7 +70,7 @@ pub fn quick_sort(v: &Vec<i32>) -> Vec<i32> {
 // no idea if that's the Rust way (???)
 pub fn vect_to_string(v: &Vec<i32>) -> String {
   let mut r = "".to_string();
-  for i in v { r = r + &i.to_string() + " " }
+  for i in v { r = r + &i.to_string() + " "; }
   r
 }
 
@@ -89,7 +89,7 @@ pub fn insert(mut v: Vec<i32>, e: i32) -> Vec<i32> {
   let n = v.len();
 
   for i in 0..n {
-    if v[n-1] < v[i] { v = swap(v, n-1, i) }
+    if v[n-1] < v[i] { v = swap(v, n-1, i); }
   }
 
   v
@@ -106,9 +106,9 @@ pub fn split(v: Vec<i32>) -> (Vec<i32>, Vec<i32>) {
 
     for i in 0..n {
       if i < n/2 {
-        left.push(v[i])
+        left.push(v[i]);
       } else {
-        right.push(v[i])
+        right.push(v[i]);
       }
     }
 
@@ -122,8 +122,8 @@ pub fn merge(mut left: Vec<i32>, mut right: Vec<i32>) -> Vec<i32> {
   let (mut i, mut j) = (0, 0);
   let (m, n) = (left.len(), right.len());
 
-  if m == 0 { return right }
-  if n == 0 { return left  }
+  if m == 0 { return right; }
+  if n == 0 { return left; }
 
   while i < m || j < n {
     if i == m  { v.push(right[j]); j=j+1; continue; }
